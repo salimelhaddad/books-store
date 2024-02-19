b9b073bf22e0cdf1b7da8615bb5a5f40060a6f0e
salimelhaddad <elhaddad2088@gmail.com>
2024-02-19 13:27:54 +0100

    feat: first component + add jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <Button />
+        <Button label="Click me" />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
